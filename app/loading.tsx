5ea153f323f4d8871176061aa4b2d99dc05050d2
Sapper <saniamykhailenko@gmail.com>
2025-10-24 16:22:15 +0300

    add loader

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,3 +1,17 @@
+import { BeatLoader } from "react-spinners";
+
 export default function Loading() {
-  return <p>Loading, please wait...</p>;
+  return (
+    <BeatLoader
+      color="#333"
+      cssOverride={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        flex: 1,
+      }}
+      margin={5}
+      size={20}
+    />
+  );
 }
